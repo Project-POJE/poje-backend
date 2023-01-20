11a0498e18e128a1c396c85bcd58294a42b2793c
Myeongsu Yu <msu122005@hs.ac.kr>
2023-01-21 02:54:13 +0900

    feat: 자격증 삭제 관련 로직 작성

--- src/test/java/com/portfolio/poje/service/MemberServiceTest.java
+++ src/test/java/com/portfolio/poje/service/MemberServiceTest.java
@@ -1,4 +1,43 @@
+package com.portfolio.poje.service;
+
+import com.portfolio.poje.controller.member.memberDto.MemberJoinRequestDto;
+import com.portfolio.poje.domain.member.Member;
+import com.portfolio.poje.repository.MemberRepository;
+import com.portfolio.poje.repository.RefreshTokenRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
-  
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private RefreshTokenRepository refreshTokenRepository;
+
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    void join_test(){
+
+
+    }
+
+
 }
