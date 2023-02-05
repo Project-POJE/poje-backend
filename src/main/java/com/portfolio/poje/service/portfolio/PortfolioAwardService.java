4ed9468373c3e158d9bf0820541604ab4c10c21e
Myeongsu Yu <msu122005@hs.ac.kr>
2023-02-05 19:14:45 +0900

    refactor: Portfolio Award 추가 및 수정 로직 변경

--- src/main/java/com/portfolio/poje/service/portfolio/PortfolioAwardService.java
+++ src/main/java/com/portfolio/poje/service/portfolio/PortfolioAwardService.java
@@ -2,7 +2,6 @@ package com.portfolio.poje.service.portfolio;
 
 import com.portfolio.poje.common.exception.ErrorCode;
 import com.portfolio.poje.common.exception.PojeException;
-import com.portfolio.poje.controller.portfolio.portfolioAwardDto.PfAwardCreateReq;
 import com.portfolio.poje.controller.portfolio.portfolioAwardDto.PfAwardDeleteReq;
 import com.portfolio.poje.controller.portfolio.portfolioAwardDto.PfAwardInfoResp;
 import com.portfolio.poje.controller.portfolio.portfolioAwardDto.PfAwardUpdateReq;
@@ -26,23 +25,28 @@ public class PortfolioAwardService {
 
 
     /**
-     * 포트폴리오 수상 정보 등록
-     * @param pfAwardCreateReq
+     * 포트폴리오 기본 수상 정보 생성
+     * @param portfolioId
+     * @return : PfAwardInfoResp
      */
     @Transactional
-    public void enroll(PfAwardCreateReq pfAwardCreateReq){
-        Portfolio portfolio = portfolioRepository.findById(pfAwardCreateReq.getPortfolioId()).orElseThrow(
+    public PfAwardInfoResp createAward(Long portfolioId){
+        Portfolio portfolio = portfolioRepository.findById(portfolioId).orElseThrow(
                 () -> new PojeException(ErrorCode.PORTFOLIO_NOT_FOUND)
         );
 
         PortfolioAward portfolioAward = PortfolioAward.enrollPortfolioAward()
-                .supervision(pfAwardCreateReq.getSupervision())
-                .grade(pfAwardCreateReq.getGrade())
-                .description(pfAwardCreateReq.getDescription())
+                .supervision("주관을 입력해주세요.")
+                .grade("순위를 입력해주세요. (e.g.3등 or 동상)")
+                .description("설명을 입력해주세요.")
                 .portfolio(portfolio)
                 .build();
 
         portfolioAwardRepository.save(portfolioAward);
+
+        return PfAwardInfoResp.builder()
+                .portfolioAward(portfolioAward)
+                .build();
     }
 
 
@@ -70,7 +74,7 @@ public class PortfolioAwardService {
      * @param pfAwardUpdateReq
      */
     @Transactional
-    public void updateAwardInfo(PfAwardUpdateReq pfAwardUpdateReq){
+    public PfAwardInfoResp updateAwardInfo(PfAwardUpdateReq pfAwardUpdateReq){
         PortfolioAward portfolioAward = portfolioAwardRepository.findById(pfAwardUpdateReq.getPortfolioAwardId()).orElseThrow(
                 () -> new PojeException(ErrorCode.AWARD_NOT_FOUND)
         );
@@ -78,6 +82,10 @@ public class PortfolioAwardService {
         portfolioAward.updateInfo(pfAwardUpdateReq.getSupervision(),
                                   pfAwardUpdateReq.getGrade(),
                                   pfAwardUpdateReq.getDescription());
+
+        return PfAwardInfoResp.builder()
+                .portfolioAward(portfolioAward)
+                .build();
     }
 
 
