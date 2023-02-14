67b9a7916489967fd895cb48c37569ba6e696d62
Myeongsu Yu <msu122005@hs.ac.kr>
2023-02-15 00:13:44 +0900

    test: 빌드 목적으로 테스트 코드 주석 처리

--- src/test/java/com/portfolio/poje/PojeApplicationTests.java
+++ src/test/java/com/portfolio/poje/PojeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.portfolio.poje;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PojeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.portfolio.poje;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PojeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
