e6413e9d83eeae6da19178f62f5954031af0ef8e
Myeongsu Yu <msu122005@hs.ac.kr>
2023-02-24 18:14:21 +0900

    refactor: 프로젝트 삭제할 이미지 목록 String 형태로 요청

--- src/main/java/com/portfolio/poje/domain/project/dto/PrImgDto.java
+++ src/main/java/com/portfolio/poje/domain/project/dto/PrImgDto.java
@@ -1,6 +1,5 @@
 package com.portfolio.poje.domain.project.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,18 +14,7 @@ public class PrImgDto {
     @NoArgsConstructor
     public static class PrImgDelListReq {
 
-        private List<PrImgDelReq> prImgDelList;
-    }
-
-
-    /**
-     * 프로젝트 이미지 삭제 요청 Dto
-     */
-    @Getter
-    @NoArgsConstructor
-    public static class PrImgDelReq {
-
-        private String prImgDelUrl;
+        private List<String> prImgDelList;
     }
 
 
--- src/main/java/com/portfolio/poje/domain/project/service/ProjectImgService.java
+++ src/main/java/com/portfolio/poje/domain/project/service/ProjectImgService.java
@@ -70,14 +70,14 @@ public class ProjectImgService {
 
         // 삭제한 경로들을 받아서 지워줌
         if (prImgDelListReq != null){
-            for (PrImgDto.PrImgDelReq prImgDelReq : prImgDelListReq.getPrImgDelList()){
+            for (String delUrl : prImgDelListReq.getPrImgDelList()){
                 // 삭제할 경로로 ProjectImg 조회
-                ProjectImg projectImg = projectImgRepository.findByUrl(prImgDelReq.getPrImgDelUrl()).orElseThrow(
+                ProjectImg projectImg = projectImgRepository.findByUrl(delUrl).orElseThrow(
                         () -> new PojeException(ErrorCode.IMG_NOT_FOUND)
                 );
 
                 // S3에서 파일 삭제
-                fileUploader.deleteFile(prImgDelReq.getPrImgDelUrl(), "project");
+                fileUploader.deleteFile(delUrl, "project");
                 // DB에서 파일 삭제
                 project.getProjectImgs().remove(projectImg);
                 projectImgRepository.delete(projectImg);
